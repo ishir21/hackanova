90bdc9cf1aaec3041ec33fc5f3517d198296189c
TCET-Hackanova <116735528+TCET-Hackanova@users.noreply.github.com>
2022-11-18 20:03:46 +0530

    Add files via upload

---
+++ assets/js/preloader.js
@@ -0,0 +1,9 @@
+const peck = document.querySelector('#preloader');
+
+window.addEventListener('load', (event) => {
+
+    function preloaderDelay() {
+        peck.style.display = "none";
+    }
+    setTimeout(preloaderDelay, 2000);
+});
